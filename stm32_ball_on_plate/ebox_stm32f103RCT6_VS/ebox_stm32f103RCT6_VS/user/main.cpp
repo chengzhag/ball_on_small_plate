6f1d8759441815c1581432dd8320d4f324c2a5dd
pidan1231239 <850576570@qq.com>
2017-06-23 21:36:44 +0800

    完成oled的显示和按键的坐标控制

--- stm32_ball_on_plate/ebox_stm32f103RCT6_VS/ebox_stm32f103RCT6_VS/user/main.cpp
+++ stm32_ball_on_plate/ebox_stm32f103RCT6_VS/ebox_stm32f103RCT6_VS/user/main.cpp
@@ -6,6 +6,9 @@
 #include "my_math.h"
 #include "signal_stream.h"
 #include "uart_vcan.h"
+#include "Button.h"
+#include "led.h"
+#include "oled_i2c.h"
 
 using namespace std;
 
@@ -35,13 +38,15 @@ UartVscan uartVscan(&uart1);
 FpsCounter fps;
 const int maxX = 128;
 const int maxY = 98;
+float posX = -1;
+float posY = -1;
 
 void posReceiveEvent(UartNum<int, 2>* uartNum)
 {
 	if (uartNum->getLength() == 2)
 	{
-		float posX = uartNum->getNum()[0];
-		float posY = uartNum->getNum()[1];
+		posX = uartNum->getNum()[0];
+		posY = uartNum->getNum()[1];
 		posX = filterX.getFilterOut(posX);
 		posY = filterY.getFilterOut(posY);
 		//uart1.printf("%f\t%f\r\n", posX, posY);
@@ -69,6 +74,13 @@ void posReceiveEvent(UartNum<int, 2>* uartNum)
 	}
 }
 
+Button keyL(&PC12, 1);
+Button keyR(&PB5, 1);
+Button keyU(&PB3, 1);
+Button keyD(&PB4, 1);
+Led led(&PD2, 1);
+OLEDI2C oled(&i2c2);
+
 void setup()
 {
 	ebox_init();
@@ -91,13 +103,75 @@ void setup()
 	pidY.setOutputUpperLimit(numeric_limits<float>::max());
 	pidY.setDesiredPoint(maxY / 2);
 	pidY.setISeperateThres(50);
+
+	keyD.begin();
+	keyL.begin();
+	keyR.begin();
+	keyU.begin();
+	led.begin();
+	oled.begin();
 }
 int main(void)
 {
 	setup();
 	//float pct = 0, increase = 1;
+	char index = 0;
+	unsigned char targetX = maxX / 2, targetY = maxY / 2;
 	while (1)
 	{
+		keyL.loop();
+		keyR.loop();
+		keyU.loop();
+		keyD.loop();
+		
+		if (keyR.click())
+		{
+			index++;
+		}
+		if (keyL.click())
+		{
+			index--;
+		}
+		limit<char>(index, 0, 1);
+
+		char increase = 0;
+		if (keyU.click())
+		{
+			increase++;
+		}
+		if (keyD.click())
+		{
+			increase--;
+		}
+		if (keyU.pressed_for(200, 0))
+		{
+			increase += 2;
+		}
+		if (keyD.pressed_for(200, 0))
+		{
+			increase -= 2;
+		}
+
+		switch (index)
+		{
+		case 0:
+			targetX += increase;
+			limit<uint8_t>(targetX, 10, maxX-10);
+			oled.printf(0, 0, 2, "*%u %u       ", targetX, targetY);
+			break;
+		case 1:
+			targetY += increase;
+			limit<uint8_t>(targetY, 10, maxY - 10);
+			oled.printf(0, 0, 2, "%u *%u       ", targetX, targetY);
+			break;
+		default:
+			break;
+		}
+
+		oled.printf(0, 2, 2, "%d %d       ", targetX, targetY);
+		pidX.setDesiredPoint(targetX);
+		pidY.setDesiredPoint(targetY);
+
 		//pct += increase;
 		//if (pct >= 100 || pct <= 0)
 		//{
@@ -108,7 +182,7 @@ int main(void)
 		//servoX.setPct(pct);
 
 		//uart1.printf("%f\r\n", servo1.getPct());
-		//delay_ms(10);
+		delay_ms(20);
 	}
 
 }
